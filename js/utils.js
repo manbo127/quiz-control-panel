d55034c295916bd6e84189e99263680eb5b6401b
manbo127 <1807029312@qq.com>
2026-06-03 09:34:40 +0800

    排行榜颜色

--- js/utils.js
+++ js/utils.js
@@ -1,18 +1,55 @@
 import { gameState } from './gameState.js';
 
-export function renderScoreboard(){
+export function renderScoreboard() {
 
-  const container = document.getElementById('scoreboard-list');
+  const container =
+    document.getElementById('scoreboard-list');
 
   container.innerHTML = '';
 
-  const sorted = [...gameState.teams].sort((a,b)=>b.score-a.score);
+  const sorted =
+    [...gameState.teams]
+      .sort((a, b) => b.score - a.score);
 
-  sorted.forEach(team=>{
+  // ===== 找前三档分数 =====
 
-    const item = document.createElement('div');
+  const scoreLevels =
+    [...new Set(
+      sorted
+        .map(team => team.score)
+        .filter(score => score > 0)
+    )];
+
+  const firstScore = scoreLevels[0];
+  const secondScore = scoreLevels[1];
+  const thirdScore = scoreLevels[2];
+
+  sorted.forEach(team => {
+
+    const item =
+      document.createElement('div');
 
     item.className = 'score-item';
+
+    if (team.score > 0) {
+
+      if (team.score === firstScore) {
+
+        item.classList.add('gold');
+
+      } else if (
+        team.score === secondScore
+      ) {
+
+        item.classList.add('silver');
+
+      } else if (
+        team.score === thirdScore
+      ) {
+
+        item.classList.add('bronze');
+      }
+    }
 
     item.innerHTML = `
       <span>${team.name}</span>
